672ac32ba9df2461850ecdd4be74500336fa88eb
dave.h <renka.shun39@gmail.com>
2017-08-08 17:06:11 +0800

    Allow disabling host tag by boolean or string false

--- spec/datadog/notifications/config_spec.rb
+++ spec/datadog/notifications/config_spec.rb
@@ -12,6 +12,21 @@ describe Datadog::Notifications::Config do
     expect(subject.tags).to eq(["custom:tag", "env:test", "host:test.host"])
   end
 
+  RSpec.shared_examples 'host tag is not picked up' do |hostname|
+    it 'should not pick up the host tag' do
+      subject.reporter = Mock::Reporter
+      subject.hostname = hostname
+      subject.tags = ["custom:tag"]
+
+      client = subject.send(:connect!)
+      expect(client).to be_instance_of(Mock::Reporter)
+      expect(subject.tags).to eq(["custom:tag", "env:test"])
+    end
+  end
+
+  include_examples 'host tag is not picked up', false
+  include_examples 'host tag is not picked up', 'false'
+
   it 'should instantiate plugins on use' do
     subject.use Datadog::Notifications::Plugins::ActionController
     expect(subject.plugins.size).to eq(1)
