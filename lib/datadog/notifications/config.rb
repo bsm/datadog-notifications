a56be59a8afb8fc8b47b6abe7a8ccf117faa97c9
dave.h <renka.shun39@gmail.com>
2017-08-08 12:40:08 +0800

    Make sure hostname is configured correctly by downward api or datadog agent
    TM-1908

--- lib/datadog/notifications/config.rb
+++ lib/datadog/notifications/config.rb
@@ -4,7 +4,7 @@ module Datadog
       attr_accessor :hostname, :namespace, :tags, :statsd_host, :statsd_port, :reporter, :plugins
 
       def initialize
-        @hostname    = ENV['INSTRUMENTATION_HOSTNAME'] || Socket.gethostname
+        @hostname    = ENV['INSTRUMENTATION_HOSTNAME']
         @statsd_host = ENV['STATSD_HOST'] || ::Datadog::Statsd::DEFAULT_HOST
         @statsd_port = (ENV['STATSD_PORT'] || ::Datadog::Statsd::DEFAULT_PORT).to_i
         @reporter    = Datadog::Notifications::Reporter
@@ -20,7 +20,7 @@ module Datadog
       def connect!
         env = ENV['RAILS_ENV'] || ENV['RACK_ENV']
         tags.push("env:#{env}")       if env && tags.none? {|t| t =~ /^env\:/ }
-        tags.push("host:#{hostname}") if tags.none? {|t| t =~ /^host\:/ }
+        tags.push("host:#{hostname}") if tags.none? {|t| t =~ /^host\:/ } && hostname
 
         reporter.new statsd_host, statsd_port, namespace: namespace, tags: tags
       end
